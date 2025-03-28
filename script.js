930488a3a41f80ff5627f8d47bb4fa9269d4f459
Mayank Kumar Gautam <mayank.guts30@gmail.com>
2025-03-28 14:18:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let clock = document.getElementById("timer");
+
+function exacttime() {
+    let currentdate = new Date();
+
+    let timeString = currentdate.toLocaleTimeString();
+
+    // clock.innerHTML = currentdate;
+    clock.innerHTML = timeString;
+}
+
+setInterval(exacttime, 1000);
